8f5b3358886989074115446456e60ce034cbcb55
aparaev <nabrosok-hd@yandex.ru>
2022-11-24 18:34:53 +0700

    cleanup

--- src/blinker-toggle_led.adb
+++
@@ -1,7 +0,0 @@
-pragma Ada_2012;
-separate (Blinker)
-procedure Toggle_LED is
-begin
-   pragma Compile_Time_Warning (Standard.True, "Toggle_LED unimplemented");
-   raise Program_Error with "Unimplemented procedure Toggle_LED";
-end Toggle_LED;
